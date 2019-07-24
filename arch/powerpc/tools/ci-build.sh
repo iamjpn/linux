5835ea412bda76652f9157f09766edee432a5a56
Michael Ellerman <mpe@ellerman.id.au>
2019-07-24 23:56:31 +1000

    CI: Go back to pulling the image

--- arch/powerpc/tools/ci-build.sh
+++ arch/powerpc/tools/ci-build.sh
@@ -1,56 +1,37 @@
 #!/bin/bash
 
-export ARCH
-export CROSS_COMPILE
-
-gcc_version=$(${CROSS_COMPILE}gcc --version | head -1)
-ld_version=$(${CROSS_COMPILE}ld --version | head -1)
-
-echo "## ARCH          = $ARCH"
-echo "## CROSS_COMPILE = $CROSS_COMPILE"
-echo "## VERSION (gcc) = $gcc_version"
-echo "## VERSION (ld)  = $ld_version"
-echo "## TIMESTAMP     = $KBUILD_BUILD_TIMESTAMP"
-
-nproc=$(nproc)
-
-if [[ -n "$PERF" ]]; then
-    (set -x; make -C tools/perf)
-    rc=$?
-elif [[ -n "$SELFTESTS" ]]; then
-    (set -x; make -j $nproc -C tools/testing/selftests TARGETS=powerpc)
-    rc=$?
-    if [[ $rc -eq 0 ]]; then
-	(set -x; make -j $nproc -C tools/testing/selftests install)
-	rc=$?
-    fi
-
-    bins=$(find $KBUILD_OUTPUT/kselftest/install -type f -perm -u+x | wc -l)
-    echo "## Found $bins selftest binaries"
-else
-    CROSS_COMPILE="ccache $CROSS_COMPILE"
-
-    echo "## DEFCONFIG     = $DEFCONFIG"
-    (set -x; make -s $DEFCONFIG)
-    rc=$?
-
-    if [[ $rc -eq 0 ]]; then
-	(set -x; make -s -j $nproc)
-	rc=$?
-    fi
-
-    if [[ $rc -eq 0 ]]; then
-	ccache -s
-	size -x -A $KBUILD_OUTPUT/vmlinux
-    fi
+if [[ -z "$TRAVIS_BUILD_DIR" || -z "$TARGET" || -z "$DISTRO" || -z "$SUBARCH" ]]; then
+    echo "Error: required environment variables not set!"
+    exit 1
 fi
 
-if [[ $rc -ne 0 ]]; then
-    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-    echo "!! Error build failed rc $rc"
-    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-else
-    echo "## Build completed OK"
-fi
+cmd="$DOCKER run --rm "
+cmd+="--network none "
+cmd+="-w /linux "
+cmd+="-v $TRAVIS_BUILD_DIR:/linux:ro "
 
-exit $rc
+cmd+="-e ARCH "
+cmd+="-e DEFCONFIG=$DEFCONFIG "
+cmd+="-e JFACTOR=$(nproc) "
+cmd+="-e KBUILD_BUILD_TIMESTAMP=$(date +%Y-%m-%d) "
+
+if [[ "$SUBARCH" == "ppc64" ]]; then
+    cross="powerpc-linux-gnu-"
+else
+    cross="powerpc64le-linux-gnu-"
+fi
+cmd+="-e CROSS_COMPILE=$cross "
+
+mkdir -p $HOME/output
+cmd+="-v $HOME/output:/output:rw "
+
+cmd+="-v $HOME/.ccache:/ccache:rw "
+cmd+="-e CCACHE_DIR=/ccache "
+cmd+="-e CCACHE=1 "
+
+cmd+="linuxppc/build:$DISTRO-$(uname -m) "
+cmd+="/bin/container-build.sh $TARGET"
+
+(set -x; $cmd)
+
+exit $?
